72610dc8327126c4bc71b8b07950f4c6d93e5feb
Robert-Maina <robert.maina@student.moringaschool.com>
2021-05-01 16:55:52 +0300

    used ducument property

--- index.js
+++ index.js
@@ -1,4 +1,34 @@
 var shedProduction = {
     shedA: 510, shedB : 308, shedC:486, shedD:572
 }
-console.log(shedProduction.shedA)
+console.log(shedProduction.shedA);
+
+var totalProduction= function(){
+    daily= shedProduction.shedA+shedProduction.shedB+shedProduction.shedC+shedProduction.shedD;
+    console.log(daily);
+}
+totalProduction();
+
+var incomeOverTime= function(selling_price, time){
+    income=(selling_price * totalProduction) * time;
+    console.log(income);
+};
+incomeOverTime(45,7);
+incomeOverTime(45,366);
+
+var monthlyIncome={
+    january:( 31 * totalProduction() ),
+    february:totalProduction()*29,
+    march:31*totalProduction(),
+    april:30*totalProduction(),
+    may:totalProduction()*31,
+    june:30*totalProduction(),
+    july:31*totalProduction(),
+    august:30*totalProduction(),
+    sept:31*totalProduction(),
+    oct:30*totalProduction(),
+    nov:31*totalProduction(),
+    dec:30*totalProduction(),
+}
+console.log(monthlyIncome.dec);
+
